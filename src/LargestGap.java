2edc0fab94965b423f9ba94857b12bf2c17544e3
Ensar_Zorlu <zorlu.qa.test@gmail.com>
2022-08-05 04:09:50 +0100

    LargestGap sorted added notes

--- src/LargestGap.java
+++ src/LargestGap.java
@@ -5,32 +5,51 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LargestGap {
+
+
+    /*
+    * Given an array of integers, return the largest gap between the sorted elements of the array.
+
+For example, consider the array:
+
+[9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5]
+... in which, after sorting, the array becomes:
+
+[0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 26]
+... so that we now see that the largest gap in the array is between 9 and 20 which is 11.
+* */
+
     public static void main(String[] args) {
 
 
-        int num[] = {9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5};
-
-
-        myMet(num);
-
+        int num[] = {13, 3, 8, 5, 5, 2, 13, 6, 14, 2, 11, 4, 10, 8, 1, 9};
+        System.out.println(myMet(num));
 
     }
 
 
+    public static int myMet(int[] num) {
 
-    public static  void myMet(int []num){
-
-        List<Integer> myList= Arrays.stream(num).boxed().collect(Collectors.toList());
+        List<Integer> myList = Arrays.stream(num).boxed().collect(Collectors.toList());
 
         Collections.sort(myList);
 
-        num= myList.stream().mapToInt(i->i).toArray();
+        num = myList.stream().mapToInt(i -> i).toArray();
 
-        System.out.println(Arrays.toString(num));
-
-
+        int max = 0;
 
 
+        for (int i = 0; i < num.length - 1; i++) {
+
+            if (max < num[i + 1] - num[i]) {
+                max = num[i + 1] - num[i];
+            }
+
+
+        }
+
+
+        return max;
     }
 
 }
