0e500d90fb474b0545c24f722007f11cefaab455
Ensar_Zorlu <zorlu.qa.test@gmail.com>
2022-08-22 16:46:52 +0100

    Meeting quastion created solved

---
+++ src/ArrayofMultiples.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class ArrayofMultiples {
+
+    /*Array of Multiples
+Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+Examples
+arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+
+arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+Notes
+Notice that num is also included in the returned array.*/
+
+    public static void main(String[] args) {
+
+
+
+
+        int newArray[]=arrayOfMultiples(7, 5);
+
+
+    }
+
+
+    public static int[] arrayOfMultiples(int num1, int num2) {
+
+
+        int arraynum[] = new int[num2];
+
+        for (int i = 0; i < arraynum.length; i++) {
+
+            arraynum[i] = num1 * (i + 1);
+
+        }
+
+
+        return arraynum;
+    }
+
+
+}
+
